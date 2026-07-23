94253510865f03aef95a1f746ad411f6d0743669
Anton Dodonov <anton.v.dodonov@gmail.com>
2026-07-23 13:43:20 +0700

    finish

--- native/Comics.Editor.Headless/obj/Debug/net10.0/Comics.Editor.Headless.AssemblyInfo.cs
+++ native/Comics.Editor.Headless/obj/Debug/net10.0/Comics.Editor.Headless.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Comics.Editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cdfa5204da54dc800f5974b401e86904eff6b6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b51081e6bf481e80eae50c089878f5b81c28629")]
 [assembly: System.Reflection.AssemblyProductAttribute("Comics.Editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Comics.Editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
